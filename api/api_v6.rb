11c1a702f0567828fbabfe55f55f5af12a8db526
Ryan B Harvey <ryan.b.harvey@gmail.com>
2013-02-02 23:36:23 -0500

    Specs still failing for API v6.  Debugging...

--- api/api_v6.rb
+++ api/api_v6.rb
@@ -1,9 +1,12 @@
 require_relative '../app/models/user.rb'
+require 'logger'
 
 module Acme
   class API_v6 < Grape::API
     version 'v6', :using => :path, :vendor => 'acme', :format => :json
     desc "Provides access to the user object, which is stored to the database."
+
+    logger = Logger.new('log/app_debug.log', 10, 2048000)
 
     resource :users do
       # GET /api/v6/users
@@ -20,6 +23,7 @@ module Acme
           logger.debug { "GET /api/v6/users -- in loop with current user = #{user.to_s}, limit = #{response_limit}" }
           response_json[:body] << { :id => user.id, :name => user.name }
         end
+        logger.debug "In API_v6#:users#get --> response_json = \"#{response_json.to_json}\" = \"#{response_json.to_s}\""
         response_json.to_json
       end
 
@@ -52,6 +56,9 @@ module Acme
 
       # DELETE /api/v6/users/:id
       desc "Delete an existing user"
+      params do
+        requires :id, type: Integer, desc: "A user ID"
+      end
       delete ':id' do
         @user = User.destroy(params[:id])
         { :id => @user.id, :status => "destroyed" }
@@ -60,6 +67,7 @@ module Acme
       # PUT /api/v6/users/:id { name = ... }
       desc "Update an existing user"
       params do
+        requires :id, type: Integer, desc: "A user ID"
         requires :name, type: String, regexp: /^[A-Z][a-zA-Z'\-]+$/, desc: "A user's name"
       end
       put ':id' do
--- spec/api/api_v6_spec.rb
+++ spec/api/api_v6_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
+logger = Logger.new('log/rspec.log', 10, 2048000)
+logger.level = Logger::DEBUG
+
 describe Acme::API do
   include Rack::Test::Methods
 
@@ -7,31 +10,58 @@ describe Acme::API do
     Acme::API
   end
 
+  logger.debug "*** Starting Acme::API testing ***"
   context "v6" do
-    context "with users" do
-      it "POST with name=John" do
-        post "/api/v6/users?name=John"
-        last_response.status.should == 201
-        last_response.body.should == { :id => 1, :name => "John", :status => "created" }.to_json
-        get "/api/v6/users/1"
-        last_response.status.should == 201
-        last_response.body.should == { :id => 1, :name => "John" }.to_json
-      end
-      it "PUT with id and name=James" do
-        put "/api/v6/users/1?name=James"
-        last_response.status.should == 200
-        last_response.body.should == { :id => 1, :name => "James", :status => "updated" }.to_json
-      end
+    logger.debug "***** In context v6 *****"
+
+    it "POST with name=John" do
+      logger.debug "***** Testing 'POST with name=John' *****"
+      post "/api/v6/users?name=John"
+      logger.debug "POST test\nlast_response = #{last_response.to_json}"
+      last_response.status.should == 201
+      logger.debug "POST test\nlast_response.body = #{last_response.body}"
+      logger.debug "POST test\nlast_response.body['id'] = #{last_response.body['id']}"
+      logger.debug "POST test\nlast_response.body['name'] = #{last_response.body['name']}"
+      last_response.body["id"].should exist
+      @test_id = last_response.body.first[:id]
+      logger.debug "  -> @test_id = #{@test_id}"
+      last_response.body["name"].should == "John"
+      last_response.body["status"].should == "created"
     end
+
+    it "GET with id" do
+      logger.debug "***** Testing 'GET with id' *****"
+      get "/api/v6/users/#{@test_id}"
+      logger.debug "GET :id test\nlast_response = #{last_response.to_json}\n@test_id = #{@test_id}"
+      last_response.status.should == 201
+      last_response.body.should == [{ :id => @test_id, :name => "John" }.to_json]
+    end
+
+    it "PUT with id and name=James" do
+      logger.debug "***** Testing 'PUT with id and name=James' *****"
+      put "/api/v6/users/#{@test_id}?name=James"
+      logger.debug "PUT :id test\nlast_response = #{last_response.to_json}\n@test_id = #{@test_id}"
+      last_response.status.should == 200
+      last_response.body.should == [{ :id => @test_id, :name => "James", :status => "updated" }.to_json]
+    end
+
     it "GET users" do
+      logger.debug "***** Testing 'GET' *****"
       get "/api/v6/users"
-      last_response.body.should == { :body => [ { :id => 1, :name => "James" } ] }.to_json
+      logger.debug "GET test\nlast_response = #{last_response.to_json}\n@test_id = #{@test_id}"
+      last_response.body[:body][:body].should include({ :id => @test_id, :name => "James" }.to_json)
     end
-    id "DELETE users with id" do
-      delete "/api/v6/users/1"
-      last_response.body.should == { :id => 1, :name => "James", :status => "destroyed" }.to_json
+
+    it "DELETE users with id" do
+      logger.debug "***** Testing 'DELETE with id' *****"
+      delete "/api/v6/users/#{@test_id}"
+      logger.debug "DELETE :id test\nlast_response = #{last_response.to_json}\n@test_id = #{@test_id}"
+      last_response.body.should == [{ :id => 1, :name => "James", :status => "destroyed" }.to_json]
     end
+
+    logger.debug "***** Leaving context v6 *****"
   end
 
+  logger.debug "*** Finished Acme::API testing ***"
 end
 
